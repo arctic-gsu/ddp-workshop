219b6590fe05f5a3df66494418c25b727be82cb8
shrutishrestha <shruti.shresthaa@gmail.com>
2024-02-09 05:57:53 +0000

    imports MyTrainDataset from datautils

--- 1_Single_node_Single_gpu.py
+++ 1_Single_node_Single_gpu.py
@@ -1,20 +1,8 @@
 import torch
 from torch.utils.data import Dataset
-
-class MyTrainDataset(Dataset):
-    def __init__(self, size):
-        self.size = size
-        self.data = [(torch.rand(20), torch.rand(1)) for _ in range(size)]
-
-    def __len__(self):
-        return self.size
-    
-    def __getitem__(self, index):
-        return self.data[index]
-
 import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
-
+from datautils import MyTrainDataset
 class Trainer:
     def __init__(
         self,
